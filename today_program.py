4f47e7c97990864b4d36fb3634b27275c0197429
KeTroN17032098 <64875428+KeTroN17032098@users.noreply.github.com>
2021-07-06 13:03:07 +0900

    Update today_program.py

--- today_program.py
+++ today_program.py
@@ -23,7 +23,8 @@ def today_date():
     return datetime.datetime.today().strftime("%Y-%m-%d")
 
 def now_time():
-    return today_date()+datetime.datetime.now().strftime(" %I:%M %p")
+    return today_date()+datetime.datetime.now().strftime(" %I:%M:%S %p")
+    
 def startTodayResult():#오늘의 통계 기록 생성 및 불러드리기
     global today_result
     todayname=today_file_path
@@ -65,42 +66,48 @@ def saveTodayResult():#기록 저장
     with open(today_file_path,'w') as json_file:
         json.dump(today_result, json_file,indent=4) 
 
-def TimeUpdate():
-    global DateEntry
-    DateEntry.delete(0,END)
-    now=now_time().center(20," ")
-    DateEntry.insert(END,now)
+class Application(Frame):
+    def __init__(self,master=None):
+        super().__init__(master)
+        self.master = master
+        self.window_set()
+        self.pack()
+        self.create_menu()
+        self.create_widgets()
 
-def updateText(interval):
-    try:
-        TimeUpdate()
-    except RuntimeError:
-        return
+    def window_set(self):
+        self.master.iconbitmap(default=icon_path)
+        self.master.resizable(False,False)
+        self.master.title("전자정보실 "+today_date()+"일 기록")
+        self.master.geometry("500x300")
 
-    threading.Timer(interval,updateText,[interval]).start()
+    def create_menu(self):
+        pass
+
+    def create_widgets(self):
+        self.fontStyle=tkFont.Font(family="Lucida Grande",size=15)
+        self.fontStyle2=tkFont.Font(family="Lucida Grande",size=25)
+        self.create_DateField()
+        
+    def create_DateField(self):
+        self.dateField=Frame(self)
+        self.dateField.pack(fill="x")
+        
+        self.DateEntry=Entry(self.dateField,font=self.fontStyle2,width=22)
+        self.DateEntry.pack()
+        self.updateDateEntry()
+    
+    def updateDateEntry(self):
+        try:
+            self.DateEntry.delete(0,END)
+            now=now_time().center(21," ")
+            self.DateEntry.insert(END,now)
+        except RuntimeError:
+            messagebox.showinfo("시간 업데이트 오류!")
+            return
+        threading.Timer(1,self.updateDateEntry).start()
 
 if __name__ == '__main__':#treeview 이용 오늘 뿐만 아니라 옛날 기록도 조회
-    startTodayResult()
-    Window=Tk()
-    Window.title("전자정보실 ["+today_date()+"] 기록")
-    Window.geometry("500x300")
-    Window.iconbitmap(default=icon_path)
-    Window.resizable(False,False)
-    
-    fontStyle=tkFont.Font(family="Lucida Grande",size=15)
-    fontStyle2=tkFont.Font(family="Lucida Grande",size=25)
-    
-    dateField=Frame(Window)
-    dateField.pack(fill="x")
-    
-    DateEntry=Entry(dateField,font=fontStyle2,width=19)
-    DateEntry.pack()
-
-    DataField=Frame(Window)
-    DataField.pack(fill="x")
-
-    DataTable=ttk.Treeview(DataField)
-
-    updateText(2)
-    
-    Window.mainloop()
+    root=Tk()
+    app=Application(master=root)
+    app.mainloop()
